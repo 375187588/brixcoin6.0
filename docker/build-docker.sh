875f89562a9edc70c0337a934766ba7bde6ea594
SA Blockchain Solutions <32270605+awsafrica@users.noreply.github.com>
2018-09-19 13:01:44 +0200

    Update build-docker.sh

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
-DOCKER_IMAGE=${DOCKER_IMAGE:-brixcoinpay/brixcoind-develop}
+DOCKER_IMAGE=${DOCKER_IMAGE:-brixcoin/brixcoind-develop}
 DOCKER_TAG=${DOCKER_TAG:-latest}
 
 BUILD_DIR=${BUILD_DIR:-.}
